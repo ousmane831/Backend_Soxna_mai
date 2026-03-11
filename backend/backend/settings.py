1d84c03ab933cb001e4d58d8230ceac8bc679003
odioud <dioufousmaneniakhar23@gmail.com>
2026-03-11 22:51:32 +0100

    Enable CORS for all origins temporarily

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,15 +59,16 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "backend.urls"
 
-CORS_ALLOWED_ORIGINS = [
-    "https://soxna-mai-kabir.vercel.app",
-    "https://soxna-mai-kabir.netlify.app",
-    "https://smkabir.netlify.app",
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8081",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://smkabir.netlify.app",
+#     "http://localhost:8080",
+#     "http://localhost:8081",
+#     "http://127.0.0.1:8080",
+#     "http://127.0.0.1:8081",
+# ]
+
+# Pour le développement et production temporaire
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 ALLOWED_HOSTS = [
